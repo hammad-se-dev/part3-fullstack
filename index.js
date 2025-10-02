4a2269d11997da78323b62383d250396c43337a5
Hammadk12345 <hammadullahk.bscssef20@iba-suk.edu.pk>
2025-10-02 16:27:04 +0500

    part 3 exercise completed

--- index.js
+++ index.js
@@ -1,63 +1,64 @@
-import express, { json } from 'express'
+import express from 'express'
+import personsData from './person.json' with { type: 'json' }
+
+let persons = [...personsData]   // use let instead of const
 
 const app = express()
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-// this pure Node.js server code is commented out and replaced with Express.js code below it
-
-// const app = http.createServer((req,res) => {
-//     res.writeHead(200, {'Content-Type': 'text/plain'})
-//     res.end(JSON.stringify(notes))
-// })
-
-
-app.get('/', (req,res) => {
-    res.send('hello world')
-})
-
-app.get('/api/notes/', (req, res) => {
-    res.json(notes);
-});
-
-app.get('/api/notes/:id', (req,res) => {
-    const id = req.params.id;
-    const note = notes.find(note => note.id === id)
-    if (note) res.json(note)
-    else res.status(404).end()
-})
-
-app.get('/api/notes/important/:important', (req,res) => {
-    // const isImportant = req.params.important === 'true'
-    const important = req.params.important === 'true' 
-    const filtered = notes.filter(note => note.important === important)
-    res.json(filtered)
-    
-})
-
-app.delete('/api/notes/:id', (req,res) => {
-  const id = req.params.id
-  notes = notes.filter(note => note.id !== id)
-  res.status(204).end()
-} )
-
 const PORT = 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+
+app.use(express.json())
+
+
+// exercise 3.1
+app.get('/api/persons', (req, res) => {
+  res.send(persons)
 })
+
+// exercise 3.2
+
+//exercise 3.3
+app.get('/api/persons/:id', (req,res) => {
+  const id = req.params.id
+  const person = persons.find(person => person.id === id)
+  res.send(person)
+})
+
+//exercise 3.4
+app.delete('/api/persons/:id', (req,res) => {
+  const id = req.params.id
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
+//exercise 3.5
+app.post('/api/persons', (req,res) => {
+  const body = req.body
+  if (body.name && body.number){
+    if (persons.find(person => person.name === body.name)){
+      res.status(400).json({error:'name must be unique'});
+    } else {
+      const person = {
+        id: Math.floor(Math.random() * 1000),
+        name: body.name,
+        number: body.number
+      }
+      persons = persons.concat(person)
+      res.status(201).json({
+        message: "contact added successfully",
+        person: person
+      })
+    }
+    
+  }
+  else {
+    res.status(400).json({error: "name or number is missing"})
+  }
+})
+
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT)
+})
+
+
